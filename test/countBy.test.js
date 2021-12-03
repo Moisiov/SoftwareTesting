c37450cb178a7b4bcce195f6e4efcb8867436994
Luukas Lusetti <luukas.lusetti@tuni.fi>
2021-12-03 16:55:38 +0200

    Initial countBy tests

---
+++ test/countBy.test.js
@@ -0,0 +1,30 @@
+import countBy from "../src/countBy.js";
+
+test('Tests array of numbers with floor function applied to keys', ()=>{
+    expect(countBy([6.1, 4.2, 6.3], Math.floor)).toBe({'4': 1, '6': 2});
+});
+
+test('Tests array of strings using property iteratee shorthand', ()=>{
+    expect(countBy(['one', 'two', 'three'], 'length')).toBe({'3': 2, '5': 1});
+});
+
+test('Tests empty array', () => {
+    expect(countBy([])).toBe({});
+});
+
+test('Tests array with empty strings', () => {
+    expect(countBy(["", "", "test"])).toBe({"": 2, "test": 1})
+});
+
+//Loadash documentation specifies countBy to throw TypeError when 
+test('Tests null value', () => {
+    expect(countBy(null)).toBe({});
+});
+
+test('Tests empty string', () => {
+    expect(countBy(null)).toBe({});
+});
+
+test('Testing string input, with added undefined as input with Math.floor operator on iteree', () => {
+    expect(countBy("123" + undefined)).toBe({1: 1, 2: 1, 3: 1, NaN: 9})
+});
