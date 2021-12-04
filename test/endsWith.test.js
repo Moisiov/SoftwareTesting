dd2f085a28f14dc5acfd562d002ba2831bf2ad0c
Valto Moisio <valto.moisio@pilvit.fi>
2021-12-04 19:34:41 +0200

    Implement basic test cases for endsWith.js

---
+++ test/endsWith.test.js
@@ -0,0 +1,21 @@
+import endsWith from '../src/endsWith'
+
+test('String ends in given character', () => {
+    expect(endsWith('abc', 'c')).toBe(true)
+})
+
+test('String does not end in given character', () => {
+    expect(endsWith('abc', 'b')).toBe(false)
+})
+
+test('String ends in given character on given position', () => {
+    expect(endsWith('abc', 'b', 2)).toBe(true)
+})
+
+test('String ends in given substring', () => {
+    expect(endsWith('abcdefg', 'efg')).toBe(true)
+})
+
+test('String ends in given substring on given position', () => {
+    expect(endsWith('abcdefg', 'cde', 5)).toBe(true)
+})
