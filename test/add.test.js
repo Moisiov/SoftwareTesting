04165315a806483311ece52762a282727ee4f802
Valto Moisio <valto.moisio@pilvit.fi>
2021-12-04 19:22:40 +0200

    Implement basic test cases for add.js

---
+++ test/add.test.js
@@ -0,0 +1,21 @@
+import add from '../src/add'
+
+test('Add two integers', () => {
+    expect(add(123, 45)).toBe(168)
+})
+
+test('Add two decimals', () => {
+    expect(add(0.003, 0.21)).toBe(0.213)
+})
+
+test('Add zero to integer', () => {
+    expect(add(0, 4)).toBe(4)
+})
+
+test('Add two zeros', () => {
+    expect(add(0, 0)).toBe(0)
+})
+
+test('Add large integers', () => {
+    expect(add(10000000000, 20000000000000)).toBe(20010000000000)
+})
