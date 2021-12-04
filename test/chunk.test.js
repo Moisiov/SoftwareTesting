23b264fe84153c66a7db61e04bba66600c6731c9
Valto Moisio <valto.moisio@pilvit.fi>
2021-12-04 17:49:59 +0200

    Add more tests for chunk.js

--- test/chunk.test.js
+++ test/chunk.test.js
@@ -7,3 +7,19 @@ test('Split array of characters to even length chunks.', () => {
 test('Leave last chunk shorter.', () => {
     expect(chunk(['a', 'b', 'c', 'd'], 3)).toStrictEqual([['a', 'b', 'c'], ['d']])
 })
+
+test('Split array with falsy values to chunks.', () => {
+    expect(chunk(['', false, null, 'test', 5, 0], 2)).toStrictEqual([['', false], [null, 'test'], [5, 0]])
+})
+
+test('Split empty array.', () => {
+    expect(chunk([], 5)).toStrictEqual([])
+})
+
+test('Call chunk with null parameter.', () => {
+    expect(chunk(null)).toStrictEqual([])
+})
+
+test('Call chunk with 0 chunk size.', () => {
+    expect(chunk(['a', 'b', 'c'], 0)).toStrictEqual([])
+})
